437b0ba2458cf47a4f82a10aa12808353c3b6237
eredaryy <adsl_8889@163.com>
2017-10-24 22:39:28 +0800

    3rd push

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,22 @@
 * @Author: eredaryy
 * @Date:   2017-10-22 15:21:13
 * @Last Modified by:   eredaryy
-* @Last Modified time: 2017-10-22 17:17:27
+* @Last Modified time: 2017-10-24 22:32:29
 */
 
 var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+//获取html-webpack-plugin参数的方法
+var getHtmlConfig = function(name,title){
+    return {
+            template: './src/view/'+name+'.html',
+            filename:'view/'+name+'.html',
+            inject:true,
+            hash:true,
+            chunks:['common',name]
+    }
+}
 var config = {
     entry: {
         'common' : ['./src/page/common/index.js'],
@@ -23,6 +34,7 @@ var config = {
     module:{
         loaders: [
             { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader","css-loader") },
+            { test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=100&name=resource/[name].[ext]' }
         ]
     },
     plugins: [
@@ -33,6 +45,9 @@ var config = {
         }),
         //把css单独打包到文件
         new ExtractTextPlugin("css/[name].css"),
+        //html文档的处理
+        new HtmlWebpackPlugin(getHtmlConfig('index')),
+        new HtmlWebpackPlugin(getHtmlConfig('login')),
     ]
 };
 
